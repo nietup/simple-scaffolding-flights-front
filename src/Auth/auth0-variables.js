ab4af309cc47d4e4b21e7a7d7568db91bffe3819
nietup <22.jakub@gmail.com>
2019-10-21 23:08:23 +0200

    Configure security

--- src/Auth/auth0-variables.js
+++ src/Auth/auth0-variables.js
@@ -1,6 +1,6 @@
 export const AUTH_CONFIG = {
     domain: 'dev-4dflot5i.eu.auth0.com',
-    clientId: 'eP31IovgeLaX6AoJecTztLeVMVILlXNs',
+    clientId: '5dUcEiWYH7UdaAG8NG5VUlCDA6Q7A8gm',
     callbackUrl: 'http://localhost:3000/callback',
     apiUrl: 'http://localhost:8080/api'
 }
